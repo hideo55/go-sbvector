c0e5ca6ce93f00870813213dde7d241131181dd2
Hideaki Ohno <hide.o.j55@gmail.com>
2015-02-02 20:47:37 +0900

    Add test patterns

--- sbvector_test.go
+++ sbvector_test.go
@@ -1,11 +1,27 @@
 package sbvector
- 
-import "testing"
-import "fmt"
- 
+
+import (
+	"testing"
+)
+
 func TestSBVector(t *testing.T) {
-    a := sbvector{}
+    a := SBVector{}
     a.set(65, true)
-    fmt.Printf("%d\n", a.get(65))
+	a.set(200, true)
+	var x = a.get(65)
+    if x != true {
+		t.Error("Expected", true, "got", x)
+	}
+	a.build(false, false)
+	var rank = a.rank(66)
+	if rank != 1 {
+		t.Error()
+	}
+	if a.select1(0) != 65 {
+		t.Error()
+	}
+	if a.select1(1) != 200 {
+		t.Error()
+	}
 }
 
