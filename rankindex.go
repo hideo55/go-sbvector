d03c474be7e374d7cd1afcad1232cc9982d63057
Hideaki Ohno <hide.o.j55@gmail.com>
2015-02-02 20:46:28 +0900

    Change variable name

--- rankindex.go
+++ rankindex.go
@@ -1,97 +1,96 @@
 package sbvector
- 
+
 const (
-    MASK_7F uint64 = 0x7F
-    MASK_FF uint64 = 0xFF
-    MASK_1F uint64 = 0x1FF
+	mask7F uint64 = 0x7F
+	maskFF uint64 = 0xFF
+	mask1F uint64 = 0x1FF
 )
- 
+
 type rankindex struct {
-    abs_ uint64
-    rel_ uint64
-}
- 
-type  rankIndexMethods interface {
-    abs() uint64
-    rel1() uint64
-    rel2() uint64
-    rel3() uint64
-    rel4() uint64
-    rel5() uint64
-    rel6() uint64
-    rel7() uint64
- 
-    set_abs(val uint64)
-    set_rel1(val uint64)
-    set_rel2(val uint64)
-    set_rel3(val uint64)
-    set_rel4(val uint64)
-    set_rel5(val uint64)
-    set_rel6(val uint64)
-    set_rel7(val uint64)
-}
- 
-func (this *rankindex) abs() uint64 {
-    return this.abs_
-}
- 
-func (this *rankindex) rel1() uint64 {
-    return ( this.rel_ & MASK_7F )
-}
- 
-func (this *rankindex) rel2() uint64 {
-    return ( ( this.rel_ >> 7 ) & MASK_FF )
-}
- 
-func (this *rankindex) rel3() uint64 {
-    return ( ( this.rel_ >> 16 ) & MASK_FF )
-}
- 
-func (this *rankindex) rel4() uint64 {
-    return ( ( this.rel_ >> 23 ) & MASK_1F )
-}
- 
-func (this *rankindex) rel5() uint64 {
-    return ( ( this.rel_ >> 32 ) & MASK_1F )
-}
- 
-func (this *rankindex) rel6() uint64 {
-    return ( ( this.rel_ >> 41 ) & MASK_1F )
-}
- 
-func (this *rankindex) rel7() uint64 {
-    return ( ( this.rel_ >> 50 ) & MASK_1F )
-}
- 
-func (this *rankindex) set_abs(val uint64) {
-    this.abs_ = val
-}
- 
-func (this *rankindex) set_rel1(val uint64) {
-    this.rel_ = ((this.rel_ & ^MASK_7F) | (val & MASK_7F))
-}
- 
-func (this *rankindex) set_rel2(val uint64) {
-    this.rel_ = ((this.rel_ & ^(MASK_FF << 7)) | ((val & MASK_FF) << 7))
-}
- 
-func (this *rankindex) set_rel3(val uint64) {
-    this.rel_ = ((this.rel_ & ^(MASK_FF << 15)) | ((val & MASK_FF) << 15))
-}
- 
-func (this *rankindex) set_rel4(val uint64) {
-    this.rel_ = ((this.rel_ & ^(MASK_1F << 23)) | ((val & MASK_1F) << 23))
-}
- 
-func (this *rankindex) set_rel5(val uint64) {
-    this.rel_ = ((this.rel_ & ^(MASK_1F << 32)) | ((val & MASK_1F) << 32))
-}
- 
-func (this *rankindex) set_rel6(val uint64) {
-    this.rel_ = ((this.rel_ & ^(MASK_1F << 41)) | ((val & MASK_1F) << 41))
-}
- 
-func (this *rankindex) set_rel7(val uint64) {
-    this.rel_ = ((this.rel_ & ^(MASK_1F << 50)) | ((val & MASK_1F) << 50))
+	absVal uint64
+	rel uint64
 }
 
+type rankIndexMethods interface {
+	abs() uint64
+	rel1() uint64
+	rel2() uint64
+	rel3() uint64
+	rel4() uint64
+	rel5() uint64
+	rel6() uint64
+	rel7() uint64
+
+	setAbs(val uint64)
+	setRel1(val uint64)
+	setRel2(val uint64)
+	setRel3(val uint64)
+	setRel4(val uint64)
+	setRel5(val uint64)
+	setRel6(val uint64)
+	setRel7(val uint64)
+}
+
+func (index *rankindex) abs() uint64 {
+	return index.absVal
+}
+
+func (index *rankindex) rel1() uint64 {
+	return (index.rel & mask7F)
+}
+
+func (index *rankindex) rel2() uint64 {
+	return ((index.rel >> 7) & maskFF)
+}
+
+func (index *rankindex) rel3() uint64 {
+	return ((index.rel >> 15) & maskFF)
+}
+
+func (index *rankindex) rel4() uint64 {
+	return ((index.rel >> 23) & mask1F)
+}
+
+func (index *rankindex) rel5() uint64 {
+	return ((index.rel >> 32) & mask1F)
+}
+
+func (index *rankindex) rel6() uint64 {
+	return ((index.rel >> 41) & mask1F)
+}
+
+func (index *rankindex) rel7() uint64 {
+	return ((index.rel >> 50) & mask1F)
+}
+
+func (index *rankindex) setAbs(val uint64) {
+	index.absVal = val
+}
+
+func (index *rankindex) setRel1(val uint64) {
+	index.rel = ((index.rel & ^mask7F) | (val & mask7F))
+}
+
+func (index *rankindex) setRel2(val uint64) {
+	index.rel = ((index.rel & ^(maskFF << 7)) | ((val & maskFF) << 7))
+}
+
+func (index *rankindex) setRel3(val uint64) {
+	index.rel = ((index.rel & ^(maskFF << 15)) | ((val & maskFF) << 15))
+}
+
+func (index *rankindex) setRel4(val uint64) {
+	index.rel = ((index.rel & ^(mask1F << 23)) | ((val & mask1F) << 23))
+}
+
+func (index *rankindex) setRel5(val uint64) {
+	index.rel = ((index.rel & ^(mask1F << 32)) | ((val & mask1F) << 32))
+}
+
+func (index *rankindex) setRel6(val uint64) {
+	index.rel = ((index.rel & ^(mask1F << 41)) | ((val & mask1F) << 41))
+}
+
+func (index *rankindex) setRel7(val uint64) {
+	index.rel = ((index.rel & ^(mask1F << 50)) | ((val & mask1F) << 50))
+}
